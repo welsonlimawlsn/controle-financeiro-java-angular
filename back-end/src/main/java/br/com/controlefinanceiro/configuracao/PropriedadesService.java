b45e132f05d0357514c744391712265b9fd4e1d4
Welson Teles <welsonlimawlsn@gmail.com>
2020-05-06 23:40:07 -0300

    Ajustes

--- back-end/src/main/java/br/com/controlefinanceiro/configuracao/PropriedadesService.java
+++ back-end/src/main/java/br/com/controlefinanceiro/configuracao/PropriedadesService.java
@@ -1,35 +1,15 @@
 package br.com.controlefinanceiro.configuracao;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
 public class PropriedadesService
 {
-    private static Properties properties;
-
-    static
-    {
-        InputStream properties = PropriedadesService.class.getClassLoader().getResourceAsStream("application.properties");
-
-        PropriedadesService.properties = new Properties();
-        try
-        {
-            PropriedadesService.properties.load(properties);
-        }
-        catch (IOException e)
-        {
-            throw new RuntimeException(e);
-        }
-    }
 
     public static String getProperty(String key)
     {
-        return properties.getProperty(key);
+        return System.getProperties().getProperty(key);
     }
 
     public interface Constantes
     {
-        String PATH_CHAVE_CRIPTOGRAFIA = "pathChaveCriptografia";
+        String PATH_CHAVE_CRIPTOGRAFIA = "br.com.escovandobit.fintips.requisicao.chavecriptografia";
     }
 }
